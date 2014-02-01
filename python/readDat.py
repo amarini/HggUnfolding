1c61997e027e2f1f71b5bc17d8e124e686fe0f13
amarini <andrea.marini@cern.ch>
2014-02-01 17:00:23 +0100

    added files

---
+++ python/readDat.py
@@ -0,0 +1,26 @@
+import sys, os, array
+
+
+def ReadDat(inputFileName):
+	R={}
+	inputFile=open(inputFileName,"r")
+	for line in inputFile:
+		line = line.split('#')[0];
+		parts=line.split('=');
+		if len(parts)<2 : continue;
+		if parts[0]=='DataFiles' or parts[0]=='RecoFiles' or parts[0]=='GenFiles' or parts[0]=='Map':
+			R[parts[0]]=[]
+			for f in parts[1].split(','):
+				R[parts[0]].append( f ) 
+		if parts[0]=='Pt' or parts[0]=='Eta' or parts[0]=='CosThetaStar' or parts[0]=='Y':
+			R[ parts[0] ] = []
+			for pt in parts[1].split(','):
+				R[ parts[0] ].append( float(pt) )
+	return R
+
+def PrintDat(dat):
+        print "--------------------------------------------------------"
+        for name in dat:
+                print "Dat Key '" +str(name) + "'   value: " + str(dat[name])
+        print "--------------------------------------------------------"
+        print 
