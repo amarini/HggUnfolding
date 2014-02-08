cfb9b862de049cfdfa979e3ec41bf863c158b448
Andrea Marini <andrea.marini@cern.ch>
2014-02-08 13:01:34 +0100

    added extra macros to readDat

--- python/readDat.py
+++ python/readDat.py
@@ -10,8 +10,9 @@ def ReadDat(inputFileName):
 		parts=line.split('=');
 		if len(parts)<2 : continue;
 		#list of string
-		if parts[0]=='DataFiles' or parts[0]=='RecoFiles' or parts[0]=='GenFiles' or parts[0]=='Map':
-			R[parts[0]]=[]
+		if parts[0]=='DataFiles' or parts[0]=='RecoFiles' or parts[0]=='GenFiles' or parts[0]=='Map' or parts[0] == 'Attribute' or parts[0] == 'Functions':
+			if parts[0] not in R:
+				R[parts[0]]=[]
 			for f in parts[1].split(','):
 				R[parts[0]].append( f.split()[0] )  # make read of \n
 		#list of float
@@ -27,7 +28,38 @@ def ReadDat(inputFileName):
 			R[ parts[0] ] = float(parts[1])
 		#string
 		if parts[0]=='OutFile' :
-			R[ parts[0] ] = parts[1]
+			R[ parts[0] ] = parts[1].split()[0] ## remove e.g. eol
+		#include -- TODO check
+		if parts[0] == "include":
+			filename2=parts[1].replace(" ","")
+			k=filename.rfind("/")
+			if k>=0:
+				dir=filename[:k] + "/"
+			else: 
+				dir="./"
+			try:
+				f=open(dir+filename2);
+				f.close();
+				print "Reading configuration from "+dir+filename2
+				Dat2=read_dat(dir+filename2)
+				for name in Dat2:
+					if (name=="Attribute" or name=="Functions" ) and name in R:
+						R[name]+=Dat2[name]
+					else:
+						R[name]=Dat2[name]
+			except IOError:
+				try:
+					f=open("./"+filename2);
+					f.close();
+					print "Reading configuration from ./"+filename2
+					Dat2=read_dat("./"+filename2)
+					for name in Dat2:
+						if (name=="Attribute" or name=="Functions" ) and name in R:
+							R[name]+=Dat2[name]
+						else:
+							R[name]=Dat2[name]
+				except IOError:
+					print "ERROR: "+dir+filename2+" - ./"+filename2+" - No such file or directory"
 	return R
 
 def PrintDat(dat):
@@ -36,3 +68,24 @@ def PrintDat(dat):
                 print "Dat Key '" +str(name) + "'   value: " + str(dat[name])
         print "--------------------------------------------------------"
         print 
+def ReadFromDat(dat,what,default,Error):
+	try:
+		return dat[what]
+	except KeyError:
+		print Error
+		return default
+def SetAttribute(Analyzer, attr,value ):
+	print "Set Attribute %s to %s"%(attr,value)
+	exec("Analyzer.%s=%s"%(attr,value))
+	return
+def SetFunction(Analyzer, attr ):
+	print "Executing Functions '%s'"%attr
+	print "Analyzer.%s"%(attr)
+	exec("Analyzer.%s"%(attr))
+	return
+def SetAttributes(Analyzer, dat):
+	for (attr,value) in dat["Attribute"]:
+		SetAttribute(Analyzer,attr,value)
+	for attr in dat["Functions"]:
+		SetFunction(Analyzer,attr)
+	return
